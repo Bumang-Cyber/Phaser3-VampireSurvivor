40e6889cc51f0512f4401af7c646df6f1f76a86e
Bumang-Cyber <calmness0729@gmail.com>
2024-10-26 01:14:04 +0900

    feat: 씬 구현, 플레이어 움직임 구현, 몹 구현

---
+++ src/characters/mob.js
@@ -0,0 +1,82 @@
+import Phaser from "phaser";
+
+export default class Mob extends Phaser.Physics.Arcade.Sprite {
+  // 씬, x, y, 스프라이트, 이름, 첫HP, 드랍률
+  constructor(scene, x, y, texture, animKey, initHp, dropRate) {
+    super(scene, x, y, texture);
+    scene.add.existing(this);
+    scene.Physics.add.existing(this);
+
+    this.play(animKey);
+
+    // css의 zIndex와 유사
+    this.setDepth(10);
+    this.scale = 2;
+
+    this.m_speed = 50;
+    this.m_hp = initHp;
+    this.m_dropRate = dropRate;
+
+    if (texture === "mob1") {
+      // <instance> setBodySize(width, height, [center])
+      this.setBodySize(24, 14, false);
+      this.setOffset(0, 14); // 바닥에 중심점을 배치
+    } else if (texture === "mob2") {
+      this.setBodySize(24, 32);
+    } else if (texture === "mob3") {
+      this.setBodySize(24, 32);
+    } else if (texture === "mob4") {
+      this.setBodySize(24, 32);
+    } else if (texture === "lion") {
+      this.setBodySize(40, 64);
+    }
+
+    const timer = this.scene.time.addEvent({
+      delay: 100,
+      callback: () => {
+        scene.physics.moveToObject(this, scene.m_player, this.m_speed);
+      },
+      loop: true,
+    });
+
+    this.m_events = [];
+    this.m_events.push(timer);
+
+    // Phaser.Scene에는 update함수가 있지만
+    // Mob은 Phaser.Physics.Arcade.Sprite를 상속한 클래스로 update 함수가 없기 때문에
+    // Scene의 update가 실행될 때마다 Mob도 update 함수가 실행되게 한다.
+
+    scene.events.on("update", (time, delta) => {
+      this.update(time, delta);
+    });
+  }
+
+  update() {
+    if (!this.body) {
+      this.scene.events.off("update", this.update, this);
+      return;
+    }
+
+    if (this.x < this.scene.m_player.x) {
+      this.flipX = true;
+    } else {
+      this.flipX = false;
+    }
+  }
+}
+
+/*
+scene.time.addEvent의 일반적인 구조:
+
+this.scene.time.addEvent({
+  delay: 시간(ms),     // 몇 밀리초 후에 실행할지 설정
+  callback: 함수,      // 실행될 함수
+  callbackScope: this, // 콜백 함수 내에서 'this'가 가리키는 범위를 설정
+  loop: true/false     // 반복 여부 설정
+});
+
+scene.time.addEvent는 Phaser의 내장함수로, 파라미터로 전달한 config객체에 따라 이벤트를 발생시킨다.
+
+moveToObject라는 Phaser 내장 함수의 파라미터는 다음과 같다.
+moveToObject(gameObject, destination, [speed], [maxTime])
+*/
---
+++ src/scenes/PlayingScene.js
@@ -0,0 +1,96 @@
+import Phaser from "phaser";
+import Player from "../characters/player";
+import { setBackground } from "../utils/backgroundManager";
+import Config from "../config";
+
+export default class PlayingScene extends Phaser.Scene {
+  constructor() {
+    super("playGame");
+  }
+
+  create() {
+    // 사용할 sound들을 추가해놓는 부분입니다.
+    // load는 전역적으로 어떤 scene에서든 asset을 사용할 수 있도록 load 해주는 것이고,
+    // add는 해당 scene에서 사용할 수 있도록 scene의 멤버 변수로 추가할 때 사용하는 것입니다.
+    this.sound.pauseOnBlur = false;
+    this.m_beamSound = this.sound.add("audio_beam");
+    this.m_scratchSound = this.sound.add("audio_scratch");
+    this.m_hitMobSound = this.sound.add("audio_hitMob");
+    this.m_growlSound = this.sound.add("audio_growl");
+    this.m_explosionSound = this.sound.add("audio_explosion");
+    this.m_expUpSound = this.sound.add("audio_expUp");
+    this.m_hurtSound = this.sound.add("audio_hurt");
+    this.m_nextLevelSound = this.sound.add("audio_nextLevel");
+    this.m_gameOverSound = this.sound.add("audio_gameOver");
+    this.m_gameClearSound = this.sound.add("audio_gameClear");
+    this.m_pauseInSound = this.sound.add("audio_pauseIn");
+    this.m_pauseOutSound = this.sound.add("audio_pauseOut");
+
+    // player를 m_player라는 멤버 변수로 추가합니다.
+    this.m_player = new Player(this);
+    // camera를 m_player를 따라가게 함
+    this.cameras.main.startFollow(this.m_player);
+
+    // PlayingScene의 background를 설정합니다.
+    setBackground(this, "background1");
+
+    this.m_cursorKeys = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    this.movePlayerManager();
+
+    /* 
+      setX와 setY를 정의한 적이 없는데 사용 가능한 이유:
+      이것들은 Phaser.GameObjects.GameObject에 있는 메소드이고,
+      Phaser의 모든 오브젝트들은 Phaser.GameObjects.GameObject를 상속하기 때문이다.
+      이를 통해 게임 내에서 게임 오브젝트의 위치, 크기, 스케일, 회전, 투명도 등을 제어할 수 있는 많은 기본 도구를 제공한다.
+    */
+    this.m_background.setX(this.m_player.x - Config.width / 2);
+    this.m_background.setY(this.m_player.y - Config.height / 2);
+
+    /*
+      Phaser.GameObjects.TileSprite에는 tilePositionX, tilePositionY 등의 메소드가 있다.
+      Config.width/2 는 그닥 중요하지 않고 this.m_player.x가 중요한 듯.
+     */
+    this.m_background.tilePositionX = this.m_player.x - Config.width / 2;
+    this.m_background.tilePositionY = this.m_player.y - Config.width / 2;
+  }
+
+  movePlayerManager() {
+    if (
+      this.m_cursorKeys.left.isDown ||
+      this.m_cursorKeys.right.isDown ||
+      this.m_cursorKeys.down.isDown ||
+      this.m_cursorKeys.up.isDown
+    ) {
+      if (!this.m_player.m_moving) {
+        // play를 중복으로 계속하지 않게 하기위해 !move일때만 if문에서 1회 실행
+        this.m_player.play("player_anim");
+      }
+      this.m_player.m_moving = true;
+    } else {
+      if (this.m_player.m_moving) {
+        this.m_player.play("player_idle");
+      }
+      this.m_player.m_moving = false;
+    }
+
+    let vector = [0, 0];
+    if (this.m_cursorKeys.left.isDown) {
+      // 가속도가 붙지않음. 왜냐하면 업데이트 할 때 마다 vector도 다시 생성되기 때문에
+      vector[0] -= 1;
+    }
+    if (this.m_cursorKeys.right.isDown) {
+      vector[0] += 1;
+    }
+    if (this.m_cursorKeys.up.isDown) {
+      vector[1] -= 1;
+    }
+    if (this.m_cursorKeys.down.isDown) {
+      vector[1] += 1;
+    }
+
+    this.m_player.move(vector);
+  }
+}
